d5b460113f7d121987141b35e7b7cf30aa7a49c7
Suhan <suhan.thirumalai@rootquotient.com>
2026-02-03 14:39:10 +0530

    add db url

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,7 +8,7 @@ dotenv.config()
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST || "localhost",
+    host: process.env.DATABASE_URL || "localhost",
     port: parseInt(process.env.DB_PORT || "5432"),
     username: process.env.DB_USERNAME || "postgres",
     password: process.env.DB_PASSWORD || "postgres",
