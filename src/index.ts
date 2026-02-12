bc5cbca1b7a46a38018c20f1255f4a5679144738
Suhan <suhan.thirumalai@rootquotient.com>
2026-02-12 16:55:42 +0530

    modify response

--- src/index.ts
+++ src/index.ts
@@ -59,7 +59,7 @@ AppDataSource.initialize().then(async () => {
     })
 
     app.listen(3000, () => {
-        console.log("Express server has started on port 3000. Open http://localhost:3000/users to see results")
+        console.log("Express server has started on port 3000!. Open http://localhost:3000/users to see results")
     })
 
 }).catch(error => console.log(error))
