db54d42de3ebca26fdc5762dfa5cdc58f5a9d5de
Suhan <suhan.thirumalai@rootquotient.com>
2026-02-26 19:14:17 +0530

    update revolte.yml

--- src/data-source.ts
+++ src/data-source.ts
@@ -8,11 +8,11 @@ dotenv.config()
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: process.env.DB_HOST || "localhost",
-    port: parseInt(process.env.DB_PORT || "5432"),
-    username: process.env.DB_USERNAME || "postgres",
-    password: process.env.DB_PASSWORD || "postgres",
-    database: process.env.DB_NAME || "test",
+    host: process.env.DATABASE_URL || "localhost",
+    port: parseInt(process.env.DATABASE_PORT || "5432"),
+    username: process.env.DATABASE_USERNAME || "postgres",
+    password: process.env.DATABASE_PASSWORD || "postgres",
+    database: process.env.DATABASE_NAME || "test",
     synchronize: true,
     logging: false,
     entities: [User, Post],
